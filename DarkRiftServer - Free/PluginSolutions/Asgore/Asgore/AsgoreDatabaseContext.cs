c77620c5e2e7a81f4b32285222e834794f9d6cb4
DemonicSlitter <alessandro.falco.biella@gmail.com>
2017-03-07 17:49:32 +0100

    Revert "qwertz"
    This reverts commit 61f3dd385282692591fe95ce2f8feaecf940072c.

--- DarkRiftServer - Free/PluginSolutions/Asgore/Asgore/AsgoreDatabaseContext.cs
+++ DarkRiftServer - Free/PluginSolutions/Asgore/Asgore/AsgoreDatabaseContext.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
-//nekwjhiokewh
+
 namespace Asgore
 {
     public class AsgoreDatabaseContext : DbContext
